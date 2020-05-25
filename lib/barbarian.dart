85c1fda15cda36461611ba599a2ddd58b8b248ac
Frank Moreno <frankmoreno1993@gmail.com>
2020-05-24 20:22:58 -0500

    removin unnecesssary null-aware

--- lib/barbarian.dart
+++ lib/barbarian.dart
@@ -2,14 +2,14 @@
 // is governed by the MIT license that can be found in the LICENSE file.
 import 'dart:convert' show json;
 
-import 'package:barbarian/ipa.dart';
-import 'package:flutter/foundation.dart';
+//import 'package:barbarian/ipa.dart';
+//import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:synchronized/synchronized.dart';
 
 part 'pale_ale.dart';
 
-typedef CustomDecode = dynamic Function(dynamic output);
+typedef CustomDecoder = dynamic Function(dynamic output);
 
 class Barbarian {
   static Barbarian _singleton;
@@ -39,15 +39,16 @@ class Barbarian {
 
   static dynamic read(
     String key, {
-    CustomDecode customDecode,
+    CustomDecoder customDecoder,
     dynamic defaultValue,
   }) {
     String first = _prefs.getString(key);
 
     if (first == null) return null;
 
-    String type = first.split('__type__')[0];
-    String value = first.split('__type__')[1];
+    final parts = first.split('__type__');
+    String type = parts[0];
+    String value = parts[1];
 
     switch (type) {
       case 'Null':
@@ -55,16 +56,16 @@ class Barbarian {
       case 'String':
         return value;
       case 'int':
-        return int.tryParse(value) ?? (defaultValue ?? null);
+        return int.tryParse(value) ?? defaultValue;
       case 'double':
-        return double.tryParse(value) ?? (defaultValue ?? null);
+        return double.tryParse(value) ?? defaultValue;
       case 'bool':
         return value == 'true';
       default:
         try {
-          return customDecode(json.decode(value)) ?? (defaultValue ?? null);
-        }//
-        catch(e) {
+          return customDecoder(json.decode(value)) ?? defaultValue;
+        } //
+        catch (e) {
           return defaultValue ?? null;
         }
     }
@@ -99,25 +100,24 @@ class Barbarian {
 
   static bool contains(String key) => _prefs.containsKey(key);
 
-
-
-  static Map<String, ValueNotifier<Lupulus>> _ipaListeners = Map();
-
-  static ValueNotifier<T> listenIpa<T extends Lupulus>(String key, [T defaultValue]) {
-    if (!_ipaListeners.containsKey(key)) {
-      _ipaListeners[key] = ValueNotifier<T>(defaultValue);
-    }
-    return _ipaListeners[key];
-  }
-
-  static void writeWithNotify<T extends Lupulus>(String key, T value) {
-    Barbarian.write(key, value);
-    _ipaListeners[key]?.value = value;
-    print('_ipaListeners[key]?.value ${_ipaListeners[key]?.value}');
-  }
-
-  static void dispose() {
-    _ipaListeners.forEach((_, v) => v.dispose());
-    _ipaListeners.clear();
-  }
+  //static Map<String, ValueNotifier<Lupulus>> _ipaListeners = Map();
+  //
+  //static ValueNotifier<T> listenIpa<T extends Lupulus>(String key,
+  //    [T defaultValue]) {
+  //  if (!_ipaListeners.containsKey(key)) {
+  //    _ipaListeners[key] = ValueNotifier<T>(defaultValue);
+  //  }
+  //  return _ipaListeners[key];
+  //}
+  //
+  //static void writeWithNotify<T extends Lupulus>(String key, T value) {
+  //  Barbarian.write(key, value);
+  //  _ipaListeners[key]?.value = value;
+  //  print('_ipaListeners[key]?.value ${_ipaListeners[key]?.value}');
+  //}
+  //
+  //static void dispose() {
+  //  _ipaListeners.forEach((_, v) => v.dispose());
+  //  _ipaListeners.clear();
+  //}
 }
