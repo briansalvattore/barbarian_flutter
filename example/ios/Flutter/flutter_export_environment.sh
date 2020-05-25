a3f2e760ff9bb95e41855737a176fe2f8b883a9c
Frank Moreno <frankmoreno1993@gmail.com>
2020-05-24 21:13:50 -0500

    examples ios files updated

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\Workspace\Flutter\SDK"
-export "FLUTTER_APPLICATION_PATH=D:\Workspace\Flutter\Brian\barbarian\example"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/frank/go/src/github.com/flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/frank/go/src/github.com/briansalvattore/barbarian_flutter/example"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=D:\Workspace\Flutter\SDK\bin\cache\artifacts\engine\ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/frank/go/src/github.com/flutter/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
